c820fcebaf4ababb32da953defbc02463f7e0e64
MarcoValeri <marcovaleri@hotmail.it>
2022-04-25 09:04:24 +0100

    Add header requirements

--- cookies-on-off.php
+++ cookies-on-off.php
@@ -1,3 +1,13 @@
 <?php
 
-// Main plugin file
+/**
+ * Plugin Name:     Cookie On Off
+ * Description:     Switch on and off cookies following GDPR and other law rules
+ * Version:         1.0.0
+ * Require at leat  5.9
+ * Require PHP:     8.0
+ * Author:          Marco Valeri
+ * Author URI       https://devmarco.com/
+ * License:         GPL v2 or later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ */
