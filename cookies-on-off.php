2e7a204cd93f8a4e0d41ae7149479c19673623d0
MarcoValeri <marcovaleri@hotmail.it>
2022-05-16 15:43:18 +0100

    Add amp script

--- cookies-on-off.php
+++ cookies-on-off.php
@@ -20,6 +20,13 @@ add_action('wp_enqueue_scripts', 'cookie_banner_js');
 
 // Add function that print out markup into the footer
 function cookie_banner() {
-    echo 'This is the cookie banner';
+    $banner = '<div>';
+    $banner .= '<amp-script script="test">';
+    $banner .= 'This is the cookie banner for deskotp and mobile</div>';
+    $banner .= '</amp-script>';
+    $banner .= '<script id="test" type="text/plain" target="amp-script">';
+    $banner .= 'console.log(`Hello JS amp`);';
+    $banner .= '</script>';
+    echo $banner;
 }
 add_action('wp_footer', 'cookie_banner');
